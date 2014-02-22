1f736beffc30da3483044e2b67ec4129391cfa87
Alvaro Cabrera <pateketrueke@gmail.com>
2014-02-22 13:27:16 -0600

    Adds demo tests

---
+++ tests/default/google-demo.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'Demo test Google': function(browser) {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[type=text]', 'nodejs')
+      .waitForElementVisible('button[name=btnG]', 1000)
+      .click('button[name=btnG]').pause(1000)
+      .assert.containsText('#ires', 'joyent/node')
+      .end();
+  }
+};
