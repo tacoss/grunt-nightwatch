27a9e2ca55243bc3d6ec7efbc1e83c03cbddc734
Alvaro Cabrera <pateketrueke@gmail.com>
2014-05-22 21:29:15 -0500

    Watch all files; fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     watch: {
       scripts: {
-        files: ['tests/*.js'],
+        files: ['tests/**/*.js'],
         tasks: ['default']
       }
     },
