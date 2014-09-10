07bc14d114b2aea66c63b0ced77d3a0c54a4455e
Alvaro Cabrera <pateketrueke@gmail.com>
2014-09-10 18:54:02 -0500

    Some formatting for settings

--- tasks/nightwatch.js
+++ tasks/nightwatch.js
@@ -22,18 +22,22 @@ module.exports = function(grunt) {
 
     var group = (arguments.length && Array.prototype.slice.call(arguments)) || ['default'],
         settings_json = $.cwd('nightwatch.json'),
-        deprecated_settings_json = $.cwd('settings.json'),
-        fake_opts = ['standalone', 'jar_path', 'jar_url'],
-        settings_opts = [
-          'globals', 'selenium', 'src_folders', 'output_folder', 'globals_path',
-          'custom_commands_path', 'custom_assertions_path', 'test_settings',
-          'launch_url', 'selenium_host', 'selenium_port', 'silent',
-          'output', 'disable_colors', 'screenshots', 'username',
-          'access_key', 'desiredCapabilities', 'exclude',
-          'filter', 'use_xpath',
-          'chrome_driver',
-          'ie_driver'
-        ];
+        deprecated_settings_json = $.cwd('settings.json');
+
+    var fake_opts = [
+      'standalone', 'jar_path', 'jar_url'
+    ];
+
+    var settings_opts = [
+      'globals', 'selenium', 'src_folders', 'output_folder', 'globals_path',
+      'custom_commands_path', 'custom_assertions_path', 'test_settings',
+      'launch_url', 'selenium_host', 'selenium_port', 'silent',
+      'output', 'disable_colors', 'screenshots', 'username',
+      'access_key', 'desiredCapabilities', 'exclude',
+      'filter', 'use_xpath',
+      'chrome_driver',
+      'ie_driver'
+    ];
 
     if ($.exists(deprecated_settings_json)) {
       $.log.error('Deprecated settings.json will not be merged (use nightwatch.json)');
